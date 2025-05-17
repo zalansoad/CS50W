0fddba046261f7921f43bdf454d2474209e81cf1
Zalan Fulop <zalan12f@gmail.com>
2025-05-17 09:39:25 +0200

    pages load

--- 3_Django/wiki/encyclopedia/views.py
+++ 3_Django/wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
@@ -7,4 +7,19 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    MD_entry = util.get_entry(title)
+    if not MD_entry:
+        return render(request, "encyclopedia/error.html",{
+            "entry": "Page not found"
+        })
+    else:
+        entry = markdown2.markdown(MD_entry)
+        return render(request, "encyclopedia/entry.html", {
+        "entry": entry
+        })
+    
+
+
 
