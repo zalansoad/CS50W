982e24e4c6edad66e5c89b00bf3dd7ac21268f33
Zalán Fülöp <zalan12f@gmail.com>
2026-01-17 15:10:35 +0100

    login

--- 8_Final_Project/capstone/Final_Project/Job_Hopper/views.py
+++ 8_Final_Project/capstone/Final_Project/Job_Hopper/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+import json
+from .models import User
 
 # Create your views here.
+def index(request):
+        return render(request, "job_hopper/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "job_hopper/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "job_hopper/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "job_hopper/register.html", {
+                "message": "Passwords must match."
+            })
+
+        if User.objects.filter(username__iexact=username).exists():
+            return render(request, "job_hopper/register.html", {
+                "message": "Username already taken."
+            })
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "job_hopper/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "job_hopper/register.html")
